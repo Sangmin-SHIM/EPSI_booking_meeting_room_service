24e872f639adbde94136bf8461fb30fbf098587a
Julien Flusin <julienflusin3@gmail.com>
2023-05-15 09:29:42 +0200

    Create models and admin pages

--- reservation_meeting_room/reservation/models.py
+++ reservation_meeting_room/reservation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    class Meta:
+        unique_together = ['room', 'start_datetime','end_datetime']
+    name = models.CharField(max_length=50,unique=True)
+    room = models.ForeignKey("room.Room", on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    comment = models.TextField()
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name} - {self.room} - {self.start_datetime} - {self.end_datetime} - {self.comment} - {self.user}"
